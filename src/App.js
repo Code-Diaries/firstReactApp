16c4c3f46bad25735c6697dfca5af533bc47a388
hafsa <hfsyln93@gmail.com>
2022-07-31 19:03:27 +0300

    delete all

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import "./scss/App.scss";
-import NavBar from "./component/NavBar";
-import Header from "./component/Header";
-import Section from "./component/Section";
-import Image   from "./component/Image";
+
 function App() {
   return (
-    <div classNameName="App">
-      <header className="head">
-        <NavBar />
-        <Header />
-      </header>
-<Section/>
-<Image/>
-    </div>
+
   );
 }
 
