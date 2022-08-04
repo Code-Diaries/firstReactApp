3612941d9468d304a9ede48920f3648a906b1773
Züleyha Kurt <netice2017@gmail.com>
2022-08-04 16:53:41 +0300

    Zunsplash:)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import "./scss/App.scss";
 import NavBar from "./component/NavBar";
 import Header from "./component/Header";
 import Section from "./component/Section";
-import Image   from "./component/Image";
+import Image from "./component/Image";
+
 function App() {
   return (
-    <div classNameName="App">
-      <header className="head">
-        <NavBar />
-        <Header />
-      </header>
-<Section/>
-<Image/>
+    <div>
+      <NavBar />
+      <Header />
+      <Section/>
+      <Image/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,30 +1,79 @@
-import React from 'react'
+import React from 'react' 
+import resim1 from '../img/download.png'
+import resim2 from '../img/search.png'
+import resim3 from '../img/visual-1.png'
+import resim4 from '../img/download  -menu.png'
+
 
 const Header = () => {
   return (
-    <div className="section">
-            <ul className="smalllist">
-                <li><a href="https://unsplash.com/"></a> Editorial</li>
-            </ul>
-            <div className="stick-two"></div>
-            <ul className="longlist">
-                <li><a href="https://unsplash.com/t/current-events">Current Events</a> </li>
-                <li><a href="https://unsplash.com/t/wallpapers">Wallpapers</a></li>
-                <li><a href="https://unsplash.com/t/3d-renders">3D Renders</a></li>
-                <li><a href="https://unsplash.com/t/textures-patterns">Textures & Patterns</a></li>
-                <li><a href="https://unsplash.com/t/experimental">Experimental</a></li>
-                <li><a href="https://unsplash.com/t/architecture">Architecture</a></li>
-                <li><a href="https://unsplash.com/t/nature">Nature</a></li>
-                <li><a href="https://unsplash.com/t/business-work">Business & Work</a></li>
-                <li><a href="https://unsplash.com/t/fashion">Fashion</a></li>
-                <li><a href="https://unsplash.com/t/film">Film</a></li>
-                <li><a href="https://unsplash.com/t/food-drink">Food & Drink</a></li>
-                <li><a href="https://unsplash.com/t/health">Health & Wellness</a></li>
-                <li><a href="https://unsplash.com/t/people">People</a></li>
-                <li><a href="https://unsplash.com/t/interiors">Interiors</a></li>
-                <li><a href="https://unsplash.com/t/street-photography">Photography</a></li>
-            </ul>
-        </div>
+    <div>
+         <header className="header"/>
+        
+            <nav className="navbar">
+        
+                <div className="home">
+                <a href="https://unsplash.com">
+                    <img src={resim1} alt="unsplash-home" title="Home" href="index.html" width="26" height="26"/>
+                </a>
+
+                </div>
+
+                 <div className="space"></div>
+
+
+                <div className="search_button">
+                <form className="form_1" >
+                    <button className="button_1"   title="Search Unsplash" > <a href="https://unsplash.com" >
+                        <img src={resim2} alt="büyütec" width="15" height="15"/></a>
+                    </button>
+
+                    <input className="input_1" type="search" title="Search Unsplash" autocapitalize="none" placeholder="Search free high-resolution Photos"  />
+                    
+                    <button className="button_2"> <a href="https://unsplash.com">
+                        <img src={resim3} alt="visual_ikon" title="Visual Search" width="20" height="20"/></a>
+                    </button>
+                    
+                </form>
+                 </div>
+                <div>
+                <ul className="list">
+                    <li> <a className="list_item" href="https://unsplash.com/explore">Explore</a> </li>
+                    <li> <a className="list_item" href="https://unsplash.com/advertise">Advertise</a></li>
+                    <li> <a className="list_item" href="https://unsplash.com/blog">Blog</a></li>
+                </ul>
+
+                </div>
+           
+                <div className="line"></div>
+                <div className="space2"></div>
+             
+                <div className="log-sign">
+                <a className="log"  href="https://unsplash.com/login">Log in</a>
+                <span>/</span>
+                <a className="log" href="https://unsplash.com/join">Sign up</a>
+
+                </div>
+
+                <div className="space3" ></div>
+                <div>
+                <button className="submit">
+                    <a className="submit_1"   type="button" href="">Submit a Photo</a>
+                </button> 
+
+                </div>
+                <div className="space4" ></div>
+
+
+                <div className="menu" >
+                <button className="menu1">
+                    <a href="https://unsplash.com"> <img src={resim4} alt="menu" width="20" height="20"/></a>
+                </button>
+
+                </div>
+
+            </nav>
+    </div>
   )
 }
 
